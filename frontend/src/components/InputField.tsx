a6f332d058e0f2c621518bacea3827ef3b141262
hcast04 <hector.castanos@estudiantat.upc.edu>
2024-06-24 18:46:28 +0200

    Initial todo-buy frontend done

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
-  return (
-    <form className='input'>
-        <input type='input' placeholder= 'Add an item!' className='input__box'/>
-        <button className='input__submit' type='submit'>
-            Add
-        </button> 
-    </form>
-  )
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-export default InputField
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a grocery"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
